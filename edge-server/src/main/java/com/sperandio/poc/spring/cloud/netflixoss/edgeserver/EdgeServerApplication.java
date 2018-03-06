4b994a530ba8fb8af85786804f1e046d2f6f0c44
Maikel Sperandio <maikelsperandio@gmail.com>
2018-03-06 16:43:40 -0300

    Configuration of edge server

--- edge-server/src/main/java/com/sperandio/poc/spring/cloud/netflixoss/edgeserver/EdgeServerApplication.java
+++ edge-server/src/main/java/com/sperandio/poc/spring/cloud/netflixoss/edgeserver/EdgeServerApplication.java
@@ -2,8 +2,12 @@ package com.sperandio.poc.spring.cloud.netflixoss.edgeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulServer
 public class EdgeServerApplication {
 
 	public static void main(String[] args) {
