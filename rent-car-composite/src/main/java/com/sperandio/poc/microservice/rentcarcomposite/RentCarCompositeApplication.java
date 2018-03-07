78fb4cb24217d3f41606807383665b60110bbb40
Maikel Sperandio <maikelsperandio@gmail.com>
2018-03-07 17:24:33 -0300

    Creation of the Composite service API

---
+++ rent-car-composite/src/main/java/com/sperandio/poc/microservice/rentcarcomposite/RentCarCompositeApplication.java
@@ -0,0 +1,12 @@
+package com.sperandio.poc.microservice.rentcarcomposite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarCompositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentCarCompositeApplication.class, args);
+	}
+}
